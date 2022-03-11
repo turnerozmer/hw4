55717db4fa080f33ab793c8cafe2abe0ee364fc0
turnerozmer <turner.ozmer@kellogg.northwestern.edu>
2022-03-11 17:28:08 +0000

    completed site, need to put on internet

--- db/migrate/20220210225337_create_places.rb
+++ db/migrate/20220210225337_create_places.rb
@@ -2,6 +2,7 @@ class CreatePlaces < ActiveRecord::Migration[7.0]
   def change
     create_table :places do |t|
       t.string :name
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_10_204546) do
 
   create_table "places", force: :cascade do |t|
     t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
